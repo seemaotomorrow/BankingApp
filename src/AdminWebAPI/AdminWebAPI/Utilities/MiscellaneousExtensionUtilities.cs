c4c1d1d7f95ef104063c9f0529e0c7a2048bbba1
Lea6327 <s3911598@student.rmit.edu.au>
2024-01-28 20:04:28 +1100

    AdminWepAPI and AdminWesite project

--- src/AdminWebAPI/AdminWebAPI/Utilities/MiscellaneousExtensionUtilities.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankingApp.Utilities;
-
-// Reference: RMIT WDT Week5-Lectorial McbaExample
-public static class MiscellaneousExtensionUtilities
-{
-    public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
-    public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
-}
